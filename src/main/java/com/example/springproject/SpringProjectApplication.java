0a70e858d6899409507c8e473465b1d3a78dafff
InimitablePioneer <seungwonpioneer@gmail.com>
2024-04-15 22:11:57 +0900

    Apr15 git validation procedure

--- src/main/java/com/example/springproject/SpringProjectApplication.java
+++ src/main/java/com/example/springproject/SpringProjectApplication.java
@@ -8,6 +8,7 @@ public class SpringProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProjectApplication.class, args);
+		//test in order to make sure iff the push works.
 	}
 
 }
